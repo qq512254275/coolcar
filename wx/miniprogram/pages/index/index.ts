d091de719519e9dbd455e02b356be06cc4e03113
Berger.Ke <berger.ke@feisu.com>
2022-07-08 18:43:40 +0800

    初始化项目

--- wx/miniprogram/pages/index/index.ts
+++ wx/miniprogram/pages/index/index.ts
@@ -4,46 +4,71 @@ const app = getApp<IAppOption>()
 
 Page({
   data: {
-    motto: 'Hello World from typescript',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    setting: {
+      skew: 0,
+      rotate: 0,
+      showLocation: true,
+      showScale: false,
+      subKey: '',
+      layerStyle: -1,
+      enableZoom: true,
+      enableScroll: true,
+      enableRotate: false,
+      showCompass: false,
+      enable3D: false,
+      enableOverlooking: false,
+      enableSatellite: false,
+      enableTraffic: false,
+    },
+    location: {
+      latitude: 23.099994,
+      longitude: 113.324520,
+    },
+    scale: 10,
+    markers: [
+      {
+      iconPath: "/resources/car.jpeg",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 20,
+      height: 20
+      },
+      {
+        iconPath: "/resources/car.jpeg",
+        id: 1,
+        latitude: 23.099994,
+        longitude: 114.324520,
+        width: 50,
+        height: 50
+        },
+    ],
   },
   // 事件处理函数
   bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs',
+    wx.redirectTo({
+      url: '../logs/logs?color=blue'
     })
   },
   onLoad() {
-    // @ts-ignore
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+    console.log('livesycle: index onLoad')
   },
-  getUserProfile() {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
+  onShow(){
+    console.log('livesycle: index onShow')
   },
-  getUserInfo(e: any) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+  onHide(){
+    console.log('livesycle: index onHide')
+  },
+  onReady(){
+    console.log('livesycle: index onReady')
+  },
+  onUnload(){
+    console.log('livesycle: index onUnload')
+  },
+  
+  onBtnTap(){
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      motto: "button change"
     })
   }
 })
